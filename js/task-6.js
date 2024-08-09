1b1f894e17cdede856b5947f079694938135a993
Anastasiia Pustova <anastasiia.pustova@web.de>
2024-08-10 00:14:30 +0200

    done tasks[1,2,3,4,5,6]

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,42 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+function createBoxes(amount) {
+  let size = 30;
+  const html = [];
+  for (let i = 0; i < amount; i++) {
+    let div = document.createElement('div');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    html.push(div);
+    size += 10;
+  }
+  boxes.append(...html);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
+controls.addEventListener('click', function (e) {
+  let count = +controlsInput.value;
+
+  if (e.target.hasAttribute('data-create')) {
+    if (count >= 1 && count <= 100) {
+      destroyBoxes();
+      createBoxes(count);
+      controlsInput.value = '';
+      count = 0;
+      controlsInput.focus();
+    }
+    controlsInput.value = '';
+    count = 0;
+    controlsInput.focus();
+  } else if (e.target.hasAttribute('data-destroy')) {
+    destroyBoxes();
+    controlsInput.value = '';
+    count = 0;
+    controlsInput.focus();
+  }
+});
